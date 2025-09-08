80423fda2b0e069fca21889882b734c5d8fc7706
Vlad Klunduk <klundukvlad40@gmail.com>
2025-09-09 00:24:31 +0200

    Initial Commit

---
+++ Goal-Tracker/Goal_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Goal_TrackerApp.swift
+//  Goal-Tracker
+//
+//  Created by Vlad Klunduk on 09/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Goal_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
