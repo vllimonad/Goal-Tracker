13f7666aa9e0a4018000d4e297cedbeb6c6127e3
Vlad Klunduk <klundukvlad40@gmail.com>
2025-12-03 18:15:16 +0100

    Refactored goals list view structure

--- Goal-Tracker/Modules/Goals/GoalsListView.swift
+++ Goal-Tracker/Modules/Goals/GoalsListView.swift
@@ -26,94 +26,139 @@ struct GoalsListView: View {
     
     var body: some View {
         NavigationStack {
-            List(goals) { goal in
-                GoalProgressView(goal: goal)
-                    .listRowSeparator(.hidden)
-                    .listRowInsets(.vertical, 0)
-                    .listRowInsets(.horizontal, 20)
-                    .listRowBackground(Color.clear)
-                    .swipeActions(edge: .trailing) {
-                        Button("goal.delete.action.title") {
-                            prepareForDeletion(goal)
-                        }
-                        .tint(.red)
-                        
-                        Button("goal.archive.action.title", role: .destructive) {
-                            archiveGoal(goal)
-                        }
-                        .tint(.orange)
-                        
-                        Button("goal.edit.action.title") {
-                            editGoal(goal)
-                        }
-                        .tint(.iconBlue)
-                    }
-                    .contextMenu {
-                        Button("goal.edit.action.title", systemImage: "pencil") {
-                            editGoal(goal)
-                        }
-                        .tint(.iconPrimary)
-                        
-                        Button("goal.archive.action.title", systemImage: "archivebox") {
-                            archiveGoal(goal)
-                        }
-                        .tint(.iconPrimary)
-                        
-                        Button(
-                            "goal.delete.action.title",
-                            systemImage: "xmark.bin",
-                            role: .destructive
-                        ) {
-                            prepareForDeletion(goal)
-                        }
-                        .tint(.red)
-                    } preview: {
-                        GoalProgressView(goal: goal)
-                            .frame(width: 300)
-                    }
-                    .onTapGesture {
-                        selectedGoal = goal
-                    }
+            List(goals) {
+                goalView(for: $0)
             }
             .background(.bgPage)
             .listRowSpacing(12)
             .listStyle(.plain)
             .navigationTitle("goals.title")
             .toolbarTitleDisplayMode(.inlineLarge)
-            .toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
-                    NavigationLink {
-                        ArchivedGoalsListView()
-                    } label: {
-                        Image(systemName: "archivebox")
-                            .foregroundStyle(.iconPrimary)
-                    }
-                }
+            .toolbar { toolBar() }
+            .navigationDestination(item: $goalToEdit) {
+                editGoalView(for: $0)
             }
-            .navigationDestination(item: $goalToEdit) { goal in
-                EditGoalView(goal: goal)
-                    .toolbarVisibility(.hidden, for: .tabBar)
+            .sheet(item: $selectedGoal) {
+                newRecordsViewSheet(for: $0)
             }
-            .sheet(item: $selectedGoal) { goal in
-                NewRecordView(goal: goal)
-                    .presentationDetents([
-                        .height(180)
-                    ])
-            }
-            .alert("delete.goal \(goalToDelete?.name ?? "")?", isPresented: $isDeleteAlertPresented) {
-                Button(role: .cancel) { }
-                Button("goals.alert.delete.action.title", role: .destructive, action: deleteGoal)
+            .alert(
+                "delete.goal \(goalToDelete?.name ?? "")?",
+                isPresented: $isDeleteAlertPresented
+            ) {
+                alertActions()
             }
             .overlay {
                 if goals.isEmpty {
-                    ContentUnavailableView(
-                        "goals.empty.title",
-                        systemImage: "zzz",
-                        description: Text("goals.empty.description")
-                    )
+                    contentUnavailableView()
                 }
             }
         }
+    }
+    
+    private func goalView(for goal: GoalModel) -> some View {
+        GoalProgressView(goal: goal)
+            .listRowSeparator(.hidden)
+            .listRowInsets(.vertical, 0)
+            .listRowInsets(.horizontal, 20)
+            .listRowBackground(Color.clear)
+            .swipeActions(edge: .trailing) {
+                swipeActions(for: goal)
+            }
+            .contextMenu {
+                contextMenu(for: goal)
+            } preview: {
+                GoalProgressView(goal: goal)
+                    .frame(width: 300)
+            }
+            .onTapGesture {
+                selectedGoal = goal
+            }
+    }
+    
+    private func editGoalView(for goal: GoalModel) -> some View {
+        EditGoalView(goal: goal)
+            .toolbarVisibility(.hidden, for: .tabBar)
+    }
+    
+    private func newRecordsViewSheet(for goal: GoalModel) -> some View {
+        NewRecordView(goal: goal)
+            .presentationDetents([
+                .height(180)
+            ])
+    }
+    
+    private func swipeActions(for goal: GoalModel) -> some View {
+        Group {
+            Button("goal.delete.action.title") {
+                prepareForDeletion(goal)
+            }
+            .tint(.red)
+            
+            Button("goal.archive.action.title", role: .destructive) {
+                archiveGoal(goal)
+            }
+            .tint(.orange)
+            
+            Button("goal.edit.action.title") {
+                editGoal(goal)
+            }
+            .tint(.iconBlue)
+        }
+    }
+    
+    private func contextMenu(for goal: GoalModel) -> some View {
+        Group {
+            Button("goal.edit.action.title", systemImage: "pencil") {
+                editGoal(goal)
+            }
+            .tint(.iconPrimary)
+            
+            Button("goal.archive.action.title", systemImage: "archivebox") {
+                archiveGoal(goal)
+            }
+            .tint(.iconPrimary)
+            
+            Button(
+                "goal.delete.action.title",
+                systemImage: "xmark.bin",
+                role: .destructive
+            ) {
+                prepareForDeletion(goal)
+            }
+            .tint(.red)
+        }
+    }
+    
+    @ToolbarContentBuilder
+    private func toolBar() -> some ToolbarContent {
+        ToolbarItem(placement: .topBarTrailing) {
+            NavigationLink {
+                ArchivedGoalsListView()
+            } label: {
+                Image(systemName: "archivebox")
+                    .foregroundStyle(.iconPrimary)
+            }
+        }
+    }
+    
+    private func alertActions() -> some View {
+        Group {
+            Button(role: .cancel) { }
+            
+            Button(
+                "goals.alert.delete.action.title",
+                role: .destructive,
+                action: deleteGoal
+            )
+        }
+    }
+    
+    private func contentUnavailableView() -> some View {
+        ContentUnavailableView(
+            "goals.empty.title",
+            systemImage: "zzz",
+            description: Text("goals.empty.description")
+        )
     }
     
     private func editGoal(_ goal: GoalModel) {
