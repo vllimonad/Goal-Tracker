f10649caea10a6e1318254ccfe929e73b8e4078f
Vlad Klunduk <klundukvlad40@gmail.com>
2025-12-03 20:00:47 +0100

    Refactored stats module structure

--- Goal-Tracker/Modules/Stats/RecordsHistory/RecordsHistoryView.swift
+++ Goal-Tracker/Modules/Stats/RecordsHistory/RecordsHistoryView.swift
@@ -12,36 +12,8 @@ struct RecordsHistoryView: View {
     var goal: GoalModel
     
     var body: some View {
-        List(goal.records) { record in
-            HStack {
-                Text("\(String(format: "%+.2f", record.value)) \(goal.unitType.abbreviation)")
-                    .font(.headline)
-                    .foregroundStyle(.textPrimary)
-                
-                Spacer()
-                
-                Text(record.date, format: .dateTime)
-                    .font(.subheadline)
-                    .foregroundStyle(.textSecondary)
-            }
-            .listRowBackground(Color.bgPrimary)
-            .listRowInsets(.vertical, 16)
-            .swipeActions {
-                Button("record.delete.action.title") {
-                    deleteRecord(record)
-                }
-                .tint(.red)
-            }
-            .contextMenu {
-                Button(
-                    "record.delete.action.title",
-                    systemImage: "xmark.bin",
-                    role: .destructive
-                ) {
-                    deleteRecord(record)
-                }
-                .tint(.red)
-            }
+        List(goal.records) {
+            recordRow(for: $0)
         }
         .scrollContentBackground(.hidden)
         .background(.bgPage)
@@ -49,6 +21,46 @@ struct RecordsHistoryView: View {
         .navigationTitle("\(goal.name) records")
         .navigationBarTitleDisplayMode(.inline)
         .toolbarVisibility(.hidden, for: .tabBar)
+    }
+    
+    private func recordRow(for record: RecordModel) -> some View {
+        HStack {
+            Text("\(String(format: "%+.2f", record.value)) \(goal.unitType.abbreviation)")
+                .font(.headline)
+                .foregroundStyle(.textPrimary)
+            
+            Spacer()
+            
+            Text(record.date, format: .dateTime)
+                .font(.subheadline)
+                .foregroundStyle(.textSecondary)
+        }
+        .listRowBackground(Color.bgPrimary)
+        .listRowInsets(.vertical, 16)
+        .swipeActions {
+            swipeActions(for: record)
+        }
+        .contextMenu {
+            contextMenu(for: record)
+        }
+    }
+    
+    private func swipeActions(for record: RecordModel) -> some View {
+        Button("record.delete.action.title") {
+            deleteRecord(record)
+        }
+        .tint(.red)
+    }
+    
+    private func contextMenu(for record: RecordModel) -> some View {
+        Button(
+            "record.delete.action.title",
+            systemImage: "xmark.bin",
+            role: .destructive
+        ) {
+            deleteRecord(record)
+        }
+        .tint(.red)
     }
     
     private func deleteRecord(_ record: RecordModel) {        
